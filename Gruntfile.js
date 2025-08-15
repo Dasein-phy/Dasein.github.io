f26ab505a9b5e4b93c86b269cfa709fbfc5b283a
Dasein-phy <70352233+Dasein-phy@users.noreply.github.com>
2025-08-15 10:16:06 +0800

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,73 +1,74 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
+  const OUTPUT_BASENAME = 'hux-blog'; // 与主题文件名保持一致，不改 head.html 也能直接用
 
-    // Project configuration.
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        uglify: {
-            main: {
-                src: 'js/<%= pkg.name %>.js',
-                dest: 'js/<%= pkg.name %>.min.js'
-            }
-        },
-        less: {
-            expanded: {
-                options: {
-                    paths: ["css"]
-                },
-                files: {
-                    "css/<%= pkg.name %>.css": "less/<%= pkg.name %>.less"
-                }
-            },
-            minified: {
-                options: {
-                    paths: ["css"],
-                    cleancss: true
-                },
-                files: {
-                    "css/<%= pkg.name %>.min.css": "less/<%= pkg.name %>.less"
-                }
-            }
-        },
-        banner: '/*!\n' +
-            ' * <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n' +
-            ' * Copyright <%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
-            ' */\n',
-        usebanner: {
-            dist: {
-                options: {
-                    position: 'top',
-                    banner: '<%= banner %>'
-                },
-                files: {
-                    src: ['css/<%= pkg.name %>.css', 'css/<%= pkg.name %>.min.css', 'js/<%= pkg.name %>.min.js']
-                }
-            }
-        },
-        watch: {
-            scripts: {
-                files: ['js/<%= pkg.name %>.js'],
-                tasks: ['uglify'],
-                options: {
-                    spawn: false,
-                },
-            },
-            less: {
-                files: ['less/*.less'],
-                tasks: ['less'],
-                options: {
-                    spawn: false,
-                }
-            },
-        },
-    });
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
 
-    // Load the plugins.
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-less');
-    grunt.loadNpmTasks('grunt-banner');
-    grunt.loadNpmTasks('grunt-contrib-watch');
+    uglify: {
+      main: {
+        src: 'js/' + OUTPUT_BASENAME + '.js',
+        dest: 'js/' + OUTPUT_BASENAME + '.min.js'
+      }
+    },
 
-    // Default task(s).
-    grunt.registerTask('default', ['uglify', 'less', 'usebanner']);
+    less: {
+      expanded: {
+        options: { paths: ['css'] },
+        files: (function () {
+          const out = {};
+          out['css/' + OUTPUT_BASENAME + '.css'] = 'less/' + OUTPUT_BASENAME + '.less';
+          return out;
+        })()
+      },
+      minified: {
+        options: { paths: ['css'], cleancss: true },
+        files: (function () {
+          const out = {};
+          out['css/' + OUTPUT_BASENAME + '.min.css'] = 'less/' + OUTPUT_BASENAME + '.less';
+          return out;
+        })()
+      }
+    },
 
+    banner:
+      '/*!\n' +
+      ' * <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n' +
+      ' * Copyright <%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
+      ' */\n',
+
+    usebanner: {
+      dist: {
+        options: { position: 'top', banner: '<%= banner %>' },
+        files: {
+          src: [
+            'css/' + OUTPUT_BASENAME + '.css',
+            'css/' + OUTPUT_BASENAME + '.min.css',
+            'js/' + OUTPUT_BASENAME + '.min.js'
+          ]
+        }
+      }
+    },
+
+    watch: {
+      scripts: {
+        files: ['js/' + OUTPUT_BASENAME + '.js'],
+        tasks: ['uglify', 'usebanner'],
+        options: { spawn: false }
+      },
+      less: {
+        files: ['less/*.less'],
+        tasks: ['less', 'usebanner'],
+        options: { spawn: false }
+      }
+    }
+  });
+
+  // Plugins
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-banner');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Default
+  grunt.registerTask('default', ['uglify', 'less', 'usebanner']);
 };
